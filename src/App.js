bec5fe5bb06d3ad5e228ae595ba9e4af64a3afc3
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-21 21:52:43 +0100

    Fetch characters feature is done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import './App.css';
 import NavBar from './Components/NavBar';
 import CharacterList from './Components/CharacterList';
 import SearchBar from './Components/SearchBar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function App() {
   const [items , setItems] = useState([]);
   const [loading , setLoading]  = useState(false);
   const [query , setQuery] = useState("");
+  useEffect(()=>{
+    const fetchItems = async () => {
+      const results = await axios.get(`/character?name=${query}`);
+      setItems(results.data.results);
+      setLoading(false);
+    }
+    fetchItems();
+  }, [query])
   return (
     <>
       <NavBar/>
