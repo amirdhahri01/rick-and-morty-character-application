0a0e645c0ab3598c04f8bf6d26ee9aa310a99002
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-21 23:21:22 +0100

    An update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import Header from './Components/Header';
-import CharacterCardsList from './Components/CharacterCardsList';
-import SearchBar from './Components/SearchBar';
-import { useEffect, useState } from 'react';
+import Header from "./Components/Header";
+import CharacterCardsList from "./Components/CharacterCardsList";
+import SearchBar from "./Components/SearchBar";
+import { useEffect, useState } from "react";
 import axios from "./Helpers/api";
-import Spinner from './Components/Spinner';
+import Spinner from "./Components/Spinner";
 function App() {
-  const [items , setItems] = useState([]);
-  const [loading , setLoading]  = useState(true);
-  const [query , setQuery] = useState("");
-  useEffect(()=>{
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState("");
+  useEffect(() => {
     const fetchItems = async () => {
-      const results = await axios.get(`/character?name=${query}`);
-      setItems(results.data.results);
-      setLoading(false);
-    }
+      try {
+        const results = await axios.get(`/character?name=${query}`);
+        setItems(results.data.results);
+        setLoading(false);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
     fetchItems();
-  }, [query])
+  }, [query]);
   return (
     <div className="app">
-      <Header/>
-      <SearchBar setQuery={(query) => {setQuery(query)}}/>
-      {loading ? <Spinner/> :  <CharacterCardsList items={items}/>}
+      <Header />
+      <SearchBar
+        setQuery={(query) => {
+          setQuery(query);
+        }}
+      />
+      {loading ? <Spinner /> : <CharacterCardsList items={items} />}
     </div>
   );
 }
