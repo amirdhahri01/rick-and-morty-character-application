c483c8e04cadbdc4417c592154dfd6ad6d553f8c
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-21 21:12:09 +0100

    An update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
-
+import NavBar from './Components/NavBar';
+import CharacterList from './Components/CharacterList';
+import SearchBar from './Components/SearchBar';
 function App() {
   return (
-    <></>
+    <>
+      <NavBar/>
+      <SearchBar/>
+      <CharacterList/>
+    </>
   );
 }
 
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from "../images/logo.png"
+
+const NavBar = () => {
+  return (
+     <nav>
+        <img src={logo} alt="Logo" />
+     </nav>
+  )
+}
+
+export default NavBar
